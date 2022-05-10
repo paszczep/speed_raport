1be6e0907e9475218a4ba4801ba500a1e66934a8
paszczep <potfurzbagien@gmail.com>
2022-05-10 23:40:03 +0200

    działa: edytowanie, zapisywanie i insert rekordów nieobecnych w bazie. brakuje guzika do ostatniego

--- speed_raport/raport_baza/tools/insert.py
+++ speed_raport/raport_baza/tools/insert.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 def insert_into_database():
     raport_df = get_raport_df().astype(str)
-    print('1', len(raport_df))
+    # print('1', len(raport_df))
 
     schema_name = 'public'
     table_name = 'zlecenia_raport'
@@ -17,13 +17,13 @@ def insert_into_database():
     raport_df = pd.concat([existing_df, raport_df])
     # print(list(zlec_df.columns))
     duplicates_cols = [el for el in raport_df.columns if el not in ['_TIMESTAMP', 'id']]
-    print('after removing', duplicates_cols)
-    print('2', len(raport_df))
+    # print('after removing', duplicates_cols)
+    # print('2', len(raport_df))
     raport_df.drop_duplicates(subset=duplicates_cols, keep=False, ignore_index=True, inplace=True)
-    print('3', len(raport_df))
+    # print('3', len(raport_df))
     existing_ids_list = existing_df['id'].to_list()
     raport_df = raport_df.loc[~(raport_df['id'].isin(existing_ids_list))]
-    print('4', len(raport_df))
+    # print('4', len(raport_df))
     raport_df.to_sql(name=table_name, con=engine, schema=schema_name, if_exists='append', index=False)
 
 
