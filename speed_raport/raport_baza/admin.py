c6c6192efc65a706a3985bbe4e398b86fd410d9e
paszczep <potfurzbagien@gmail.com>
2022-05-23 18:14:01 +0200

    premie + osoby

--- speed_raport/raport_baza/admin.py
+++ speed_raport/raport_baza/admin.py
@@ -12,7 +12,8 @@ class PremieAdmin(admin.ModelAdmin):
     list_display_links = list_display
     list_filter = ('spedytor', )
     ordering = ('add_date', 'spedytor', 'kwota_premii',)
-    search_fields = ('spedytor',)
+    search_fields = ('spedytor', 'nr_zlecenia')
+    raw_id_fields = ('nr_zlecenia',)
     list_per_page = 10
 
 
@@ -23,7 +24,7 @@ class OsobyAdmin(admin.ModelAdmin):
     list_filter = ('spedytor',)
     ordering = ('spedytor',)
     search_fields = ('spedytor',)
-    list_per_page = 10
+    list_per_page = 50
 
 
 # @admin.register(ZleceniaRaport)
@@ -33,6 +34,7 @@ class ZleceniaAdmin(admin.ModelAdmin):
     # list_editable = ['spedytor', 'opiekun', 'za_miejsce', 'za_data', 'wy_data', 'wy_miejsce', 'wy_miasto', 'wy_kraj', 'wy_kod', 'za_miasto', 'za_kraj', 'za_kod', 'trasa', 'opis', 'informacje', 'data_wystawienia_koszt', 'data_platnosci_koszt', 'data_wystawienia_przych', 'data_platnosci_przych', 'netto_pln_przych', 'netto_pln_koszt', 'noty_netto_pln', 'saldo_netto']
     list_filter = ('field_timestamp', 'spedytor', 'opiekun', 'opis', 'za_data')
     ordering = ('field_timestamp', 'nr_zlecenia',)
+    readonly_fields = ('id', 'field_timestamp', )
     search_fields = ('id', 'nr_zlecenia', 'spedytor', 'opiekun', )
     list_per_page = 10
     change_list_template = "zlecenia_changelist.html"
---
+++ speed_raport/tools/insert_osoby.py
@@ -0,0 +1,56 @@
+import pymssql
+import pandas as pd
+from sqlalchemy import create_engine
+from connect import *
+
+
+def dataframe_from_query(given_cursor, given_query):
+    given_cursor.execute(given_query)
+    return_data = given_cursor.fetchall()
+    dfr_columns = [item[0] for item in given_cursor.description]
+    return_dataframe = pd.DataFrame(data=return_data, columns=dfr_columns)
+
+    return return_dataframe
+
+
+def get_output_engine():
+    engine_str = f"postgresql://{OUT_DB['user']}:{OUT_DB['password']}@{OUT_DB['host']}:{OUT_DB['port']}/{OUT_DB['database']}"
+    engine = create_engine(engine_str,
+                           encoding='ISO-8859-2',
+                           # encoding='UTF-8'
+                          )
+    return engine
+
+
+conn_speed = pymssql.connect(
+    server='10.100.200.3',
+    port='1433',
+    user='pawel',
+    password='20Al3Mot@VP@weL22#',
+    database='SPEED',
+    charset='CP1250')
+cursor_speed = conn_speed.cursor()
+
+spedytorzy_query = """
+   SELECT [SPEDYTOR], [OPIEKUN] FROM [SPEED].[dbo].[ZLECENIA]
+"""
+
+spedytorzy_df = dataframe_from_query(cursor_speed, spedytorzy_query)
+
+spedytorzy = spedytorzy_df.SPEDYTOR.append(spedytorzy_df.OPIEKUN)
+
+spedytorzy_list = [sped for sped in spedytorzy.unique() if sped not in [None, '', '-']]
+unique_sped_df = pd.DataFrame(data=spedytorzy_list, columns=['spedytor'])
+
+OUT_DB = {
+    'host': '10.100.200.3',
+    'port': '5432',
+    'database': "postgres",
+    'user': "postgres",
+    # 'password': "DbMot!v@SerWBaza22#",
+    'password': "S4FVVyqlkPexcBryOx7Q5hVMuDY70eQl"}
+
+schema_name = 'public'
+table_name = 'spedytorzy_osoby'
+engine = get_output_engine()
+unique_sped_df.to_sql(name=table_name, con=engine, schema=schema_name, if_exists='append', index=False)
